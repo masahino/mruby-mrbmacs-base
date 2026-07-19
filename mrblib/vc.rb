f46153f820eaa1f3e47c8707a2107872506e7ae0
masahino <masahino@gmail.com>
2026-07-20 08:46:45 +0900

    Add Git repository discovery to VC

--- mrblib/vc.rb
+++ mrblib/vc.rb
@@ -1,19 +1,67 @@
 module Mrbmacs
-  # Version Control
+  # Version control information for a working directory.
   class VC
-    def initialize(directory)
-      @type = 'git'
-      @branch = `git -C #{directory} branch --show-current 2> /dev/null`.chomp
+    attr_reader :type, :root_directory, :branch, :state
+
+    def initialize(directory, runner = nil)
+      @directory = File.expand_path(directory)
+      @runner = runner || method(:run_git)
+      @type = nil
+      @root_directory = nil
+      @branch = ''
+      @state = :unmanaged
+      discover
+    end
+
+    def managed?
+      @state == :managed
+    end
+
+    def detached?
+      managed? && @branch.start_with?('@')
     end
 
     def to_s
-      if @branch != ''
-        "Git:#{@branch}"
+      managed? ? "Git:#{@branch}" : ''
+    end
+
+    private
+
+    def discover
+      root, status = execute('rev-parse --show-toplevel')
+      unless status == 0
+        @state = status == 127 ? :unavailable : :unmanaged
+        return
+      end
+
+      @type = :git
+      @root_directory = root.chomp
+      @state = :managed
+      branch, branch_status = execute('symbolic-ref --quiet --short HEAD')
+      if branch_status == 0
+        @branch = branch.chomp
       else
-        @branch
+        revision, revision_status = execute('rev-parse --short HEAD')
+        @branch = revision_status == 0 ? "@#{revision.chomp}" : '@unknown'
       end
     end
-  end
-  class Application
+
+    def execute(command)
+      @runner.call(@directory, command)
+    end
+
+    # Commands passed here are fixed internally; changing directory avoids
+    # interpolating a user-controlled path into the shell command.
+    def run_git(directory, command)
+      output = ''
+      status = nil
+      Dir.chdir(directory) do
+        output = `git #{command} 2>&1`
+        status = $?
+      end
+      [output, status.nil? ? 1 : status.exitstatus]
+    rescue StandardError => e
+      [e.to_s, 1]
+    end
   end
 end
--- test/vc.rb
+++ test/vc.rb
@@ -1,9 +1,48 @@
-assert('VC.new') do
-  vcinfo = Mrbmacs::VC.new(File.dirname(__FILE__))
-  assert_kind_of(Mrbmacs::VC, vcinfo)
+def vc_runner(results)
+  proc do |_directory, command|
+    results[command] || ['', 1]
+  end
 end
 
-assert('VC.to_s') do
-  vcinfo = Mrbmacs::VC.new(File.dirname(__FILE__))
-  assert_kind_of(String, vcinfo.to_s)
+assert('VC managed repository') do
+  runner = vc_runner(
+    'rev-parse --show-toplevel' => ["/work/project\n", 0],
+    'symbolic-ref --quiet --short HEAD' => ["main\n", 0]
+  )
+  vcinfo = Mrbmacs::VC.new('.', runner)
+
+  assert_true vcinfo.managed?
+  assert_equal :git, vcinfo.type
+  assert_equal '/work/project', vcinfo.root_directory
+  assert_equal 'main', vcinfo.branch
+  assert_equal 'Git:main', vcinfo.to_s
+end
+
+assert('VC detached HEAD') do
+  runner = vc_runner(
+    'rev-parse --show-toplevel' => ["/work/project\n", 0],
+    'symbolic-ref --quiet --short HEAD' => ['', 1],
+    'rev-parse --short HEAD' => ["1a2b3c4\n", 0]
+  )
+  vcinfo = Mrbmacs::VC.new('.', runner)
+
+  assert_true vcinfo.detached?
+  assert_equal '@1a2b3c4', vcinfo.branch
+  assert_equal 'Git:@1a2b3c4', vcinfo.to_s
+end
+
+assert('VC unmanaged directory') do
+  vcinfo = Mrbmacs::VC.new('.', vc_runner('rev-parse --show-toplevel' => ['', 128]))
+
+  assert_false vcinfo.managed?
+  assert_equal :unmanaged, vcinfo.state
+  assert_equal '', vcinfo.to_s
+end
+
+assert('VC git unavailable') do
+  vcinfo = Mrbmacs::VC.new('.', vc_runner('rev-parse --show-toplevel' => ['', 127]))
+
+  assert_false vcinfo.managed?
+  assert_equal :unavailable, vcinfo.state
+  assert_equal '', vcinfo.to_s
 end
