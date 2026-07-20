985ac2cd259eb4c4058889a9e6dec43bf55728de
masahino <masahino@gmail.com>
2026-07-20 09:34:28 +0900

    Add vc-diff command

--- mrblib/vc.rb
+++ mrblib/vc.rb
@@ -25,10 +25,22 @@ module Mrbmacs
       managed? ? "Git:#{@branch}" : ''
     end
 
+    def diff(filename)
+      return ['', 1] unless managed?
+
+      path = File.expand_path(filename)
+      root = @root_directory
+      prefix = root.end_with?(File::SEPARATOR) ? root : "#{root}#{File::SEPARATOR}"
+      return ["#{path} is outside the repository", 1] unless path.start_with?(prefix)
+
+      relative_path = path[prefix.length..-1]
+      execute(['diff', '--no-ext-diff', 'HEAD', '--', relative_path], root)
+    end
+
     private
 
     def discover
-      root, status = execute('rev-parse --show-toplevel')
+      root, status = execute(['rev-parse', '--show-toplevel'])
       unless status == 0
         @state = status == 127 ? :unavailable : :unmanaged
         return
@@ -37,31 +49,69 @@ module Mrbmacs
       @type = :git
       @root_directory = root.chomp
       @state = :managed
-      branch, branch_status = execute('symbolic-ref --quiet --short HEAD')
+      branch, branch_status = execute(['symbolic-ref', '--quiet', '--short', 'HEAD'])
       if branch_status == 0
         @branch = branch.chomp
       else
-        revision, revision_status = execute('rev-parse --short HEAD')
+        revision, revision_status = execute(['rev-parse', '--short', 'HEAD'])
         @branch = revision_status == 0 ? "@#{revision.chomp}" : '@unknown'
       end
     end
 
-    def execute(command)
-      @runner.call(@directory, command)
+    def execute(arguments, directory = @directory)
+      @runner.call(directory, arguments)
     end
 
-    # Commands passed here are fixed internally; changing directory avoids
-    # interpolating a user-controlled path into the shell command.
-    def run_git(directory, command)
-      output = ''
-      status = nil
+    def run_git(directory, arguments)
+      reader, writer = IO.pipe
+      pid = nil
       Dir.chdir(directory) do
-        output = `git #{command} 2>&1`
-        status = $?
+        pid = Process.spawn('git', *arguments, out: writer.fileno, err: writer.fileno)
       end
-      [output, status.nil? ? 1 : status.exitstatus]
+      writer.close
+      output = reader.read
+      reader.close
+      Process.waitpid(pid)
+      [output, $?.exitstatus]
     rescue StandardError => e
+      writer.close unless writer.nil? || writer.closed?
+      reader.close unless reader.nil? || reader.closed?
       [e.to_s, 1]
+    end
+  end
+
+  module Command
+    def vc_diff
+      source_buffer = @current_buffer
+      if source_buffer.filename == ''
+        message 'Buffer is not visiting a file'
+        return
+      end
+
+      vcinfo = source_buffer.vcinfo || VC.new(source_buffer.directory)
+      unless vcinfo.managed?
+        message 'File is not in a Git repository'
+        return
+      end
+
+      output, status = vcinfo.diff(source_buffer.filename)
+      if status != 0
+        message(output.chomp == '' ? 'Git diff failed' : output.chomp)
+        return
+      end
+      if output == ''
+        message 'No differences'
+        return
+      end
+
+      buffer_name = '*vc-diff*'
+      setup_result_buffer(buffer_name)
+      @frame.view_win.sci_set_read_only(0)
+      @frame.view_win.sci_set_text(output)
+      @current_buffer.mode = DiffMode.instance
+      update_buffer_mode(@current_buffer)
+      @frame.view_win.sci_set_save_point
+      @frame.view_win.sci_set_read_only(1)
     end
   end
 end
--- test/vc.rb
+++ test/vc.rb
@@ -1,13 +1,20 @@
 def vc_runner(results)
-  proc do |_directory, command|
-    results[command] || ['', 1]
+  proc do |_directory, arguments|
+    results[arguments] || ['', 1]
+  end
+end
+
+def recording_vc_runner(results, calls)
+  proc do |directory, arguments|
+    calls << [directory, arguments]
+    results[arguments] || ['', 1]
   end
 end
 
 assert('VC managed repository') do
   runner = vc_runner(
-    'rev-parse --show-toplevel' => ["/work/project\n", 0],
-    'symbolic-ref --quiet --short HEAD' => ["main\n", 0]
+    ['rev-parse', '--show-toplevel'] => ["/work/project\n", 0],
+    ['symbolic-ref', '--quiet', '--short', 'HEAD'] => ["main\n", 0]
   )
   vcinfo = Mrbmacs::VC.new('.', runner)
 
@@ -20,9 +27,9 @@ end
 
 assert('VC detached HEAD') do
   runner = vc_runner(
-    'rev-parse --show-toplevel' => ["/work/project\n", 0],
-    'symbolic-ref --quiet --short HEAD' => ['', 1],
-    'rev-parse --short HEAD' => ["1a2b3c4\n", 0]
+    ['rev-parse', '--show-toplevel'] => ["/work/project\n", 0],
+    ['symbolic-ref', '--quiet', '--short', 'HEAD'] => ['', 1],
+    ['rev-parse', '--short', 'HEAD'] => ["1a2b3c4\n", 0]
   )
   vcinfo = Mrbmacs::VC.new('.', runner)
 
@@ -32,7 +39,7 @@ assert('VC detached HEAD') do
 end
 
 assert('VC unmanaged directory') do
-  vcinfo = Mrbmacs::VC.new('.', vc_runner('rev-parse --show-toplevel' => ['', 128]))
+  vcinfo = Mrbmacs::VC.new('.', vc_runner(['rev-parse', '--show-toplevel'] => ['', 128]))
 
   assert_false vcinfo.managed?
   assert_equal :unmanaged, vcinfo.state
@@ -40,9 +47,75 @@ assert('VC unmanaged directory') do
 end
 
 assert('VC git unavailable') do
-  vcinfo = Mrbmacs::VC.new('.', vc_runner('rev-parse --show-toplevel' => ['', 127]))
+  vcinfo = Mrbmacs::VC.new('.', vc_runner(['rev-parse', '--show-toplevel'] => ['', 127]))
 
   assert_false vcinfo.managed?
   assert_equal :unavailable, vcinfo.state
   assert_equal '', vcinfo.to_s
+end
+
+assert('VC diff uses a repository-relative path') do
+  calls = []
+  runner = recording_vc_runner({
+    ['rev-parse', '--show-toplevel'] => ["/work/project\n", 0],
+    ['symbolic-ref', '--quiet', '--short', 'HEAD'] => ["main\n", 0],
+    ['diff', '--no-ext-diff', 'HEAD', '--', 'lib/file name.rb'] => ["diff output\n", 0]
+  }, calls)
+  vcinfo = Mrbmacs::VC.new('/work/project/lib', runner)
+
+  assert_equal ["diff output\n", 0], vcinfo.diff('/work/project/lib/file name.rb')
+  assert_equal '/work/project', calls.last[0]
+end
+
+assert('VC diff rejects a path outside the repository') do
+  runner = vc_runner(
+    ['rev-parse', '--show-toplevel'] => ["/work/project\n", 0],
+    ['symbolic-ref', '--quiet', '--short', 'HEAD'] => ["main\n", 0]
+  )
+  vcinfo = Mrbmacs::VC.new('/work/project', runner)
+  output, status = vcinfo.diff('/work/another/file.rb')
+
+  assert_equal 1, status
+  assert_include output, 'outside the repository'
+end
+
+assert('vc_diff displays the current file diff') do
+  runner = vc_runner(
+    ['rev-parse', '--show-toplevel'] => ["/work/project\n", 0],
+    ['symbolic-ref', '--quiet', '--short', 'HEAD'] => ["main\n", 0],
+    ['diff', '--no-ext-diff', 'HEAD', '--', 'lib/file.rb'] => ["diff output\n", 0]
+  )
+  app = Mrbmacs::TestSupport::Application.new
+  app.current_buffer.filename = '/work/project/lib/file.rb'
+  app.current_buffer.directory = '/work/project/lib'
+  app.current_buffer.vcinfo = Mrbmacs::VC.new('/work/project/lib', runner)
+
+  app.vc_diff
+
+  assert_equal '*vc-diff*', app.current_buffer.name
+  assert_equal 'diff', app.current_buffer.mode.name
+end
+
+assert('vc_diff reports no differences') do
+  runner = vc_runner(
+    ['rev-parse', '--show-toplevel'] => ["/work/project\n", 0],
+    ['symbolic-ref', '--quiet', '--short', 'HEAD'] => ["main\n", 0],
+    ['diff', '--no-ext-diff', 'HEAD', '--', 'lib/file.rb'] => ['', 0]
+  )
+  app = Mrbmacs::TestSupport::Application.new
+  app.current_buffer.filename = '/work/project/lib/file.rb'
+  app.current_buffer.directory = '/work/project/lib'
+  app.current_buffer.vcinfo = Mrbmacs::VC.new('/work/project/lib', runner)
+
+  app.vc_diff
+
+  assert_equal 'No differences', app.frame.echo_message
+end
+
+assert('vc_diff rejects a buffer not visiting a file') do
+  app = Mrbmacs::TestSupport::Application.new
+
+  app.vc_diff
+
+  assert_equal 'Buffer is not visiting a file', app.frame.echo_message
 end
