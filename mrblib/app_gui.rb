806fda8a441e63c7239a93130edfc3b3752f903a
masahino <masahino@gmail.com>
2026-09-05 07:50:06 +0900

    introduce ApplicationGui

---
+++ mrblib/app_gui.rb
@@ -0,0 +1,6 @@
+module Mrbmacs
+  # Application class for callback-driven (GUI) frontends: cocoa, gtk.
+  # Intermediate tier parallel to ApplicationTerminal.
+  class ApplicationGui < Application
+  end
+end
